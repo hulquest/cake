ecd0e26d52d83789910aac5da0aea68f4ed6150b
Jacob Weinstock <jakobweinstock@gmail.com>
2020-04-20 19:38:10 +0000

    move config spec to another PR

--- pkg/providers/vsphere/types.go
+++ pkg/providers/vsphere/types.go
@@ -18,27 +18,3 @@ type Infrastructure struct {
 	ResourcePool *object.ResourcePool
 	Network      object.NetworkReference
 }
-
-// ConfigSpec holds information needed to provision a K8s management cluster with the vsphere provider
-type ConfigSpec struct {
-	ProviderVsphere ProviderVsphere `yaml:"ProviderVsphere,omitempty" json:"providervsphere,omitempty"`
-}
-
-// ProviderVsphere is vsphere specifc data
-type ProviderVsphere struct {
-	URL               string  `yaml:"URL" json:"url"`
-	Username          string  `yaml:"Username" json:"username"`
-	Password          string  `yaml:"Password" json:"password"`
-	Datacenter        string  `yaml:"Datacenter" json:"datacenter"`
-	ResourcePool      string  `yaml:"ResourcePool" json:"resourcepool"`
-	Datastore         string  `yaml:"Datastore" json:"datastore"`
-	ManagementNetwork string  `yaml:"ManagementNetwork" json:"managementnetwork"`
-	StorageNetwork    string  `yaml:"StorageNetwork" json:"storagenetwork"`
-	OVA               OVASpec `yaml:"OVA" json:"ova"`
-}
-
-// OVASpec sets OVA information used for virtual machine templates
-type OVASpec struct {
-	NodeTemplate         string `yaml:"NodeTemplate" json:"nodetemplate"`
-	LoadbalancerTemplate string `yaml:"LoadbalancerTemplate" json:"loadbalancertemplate"`
-}
